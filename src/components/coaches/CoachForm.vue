fb041a40df287012b0dfd2623d17a5e363175ed6
Sambert <sambert@smartshore.nl>
2023-02-18 15:31:37 +0530

    added coach form

---
+++ src/components/coaches/CoachForm.vue
@@ -0,0 +1,27 @@
+<template>
+    <form>
+        <div class="form-control">
+            <label for="firstname">Firstname</label>
+            <input type="text" id="firstname">
+        </div>
+        <div class="form-control">
+            <label for="lastname">Lastname</label>
+            <input type="text" id="lastname">
+        </div>
+        <div class="form-control">
+            <label for="description">Description</label>
+            <textarea type="text" id="description"></textarea>
+        </div>
+        <div class="form-control">
+            <label for="rate">Hourly Rate</label>
+            <input type="number" id="rate">
+        </div>
+        <div class="form-control">
+            <h3>Area od Expertise</h3>          
+            <div>
+                <label for="rate">Hourly Rate</label>
+                <input type="number" id="rate">
+            </div> 
+        </div>
+    </form>
+</template>
