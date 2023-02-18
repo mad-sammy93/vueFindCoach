9fe795fc5143e0416396f49d4dfbb64ffbdde4c9
Sambert <sambertr@gmail.com>
2023-02-18 22:53:51 +0530

    244 fix

--- src/components/coaches/CoachForm.vue
+++ src/components/coaches/CoachForm.vue
@@ -48,7 +48,7 @@ export default {
             areas: [],
         }
     },
-    method: {
+    methods: {
         submitForm() {
             const formData = {
                 first: this.firstName,
