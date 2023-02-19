4450cd12063ab63c41abca91e21cee39acc0b5ce
Sambert <sambertr@gmail.com>
2023-02-19 20:57:15 +0530

    258 FINAL NOt found page

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,3 +1,10 @@
 <template>
-    NOTHING FOUND
+    <section>
+        <base-card>
+            <h2>Page Not Found</h2>
+            <p>
+                This page could not be found - Check out all our <router-link to="/coaches">Coaches</router-link>
+            </p>
+        </base-card>
+    </section>
 </template>
